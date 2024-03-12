0f09518d93eec021ad871beb4992102b213eac3b
GigiLi89 <li.gigi89@gmail.com>
2024-03-12 12:33:00 +0000

    Add document holders and event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+
+//Event Listener added, it's going to listen for the DOM content to be loaded..
+//..then it will execute the function in that code block
+document.addEventListener("DOMContentLoaded", function() {
+//Adding the Event Listeners below by targetting all the button-tags:
+    let buttons = document.getElementsByTagName("button");
+//Takes all buttons array and return each element and store in variable button on each iteration
+    for (let button of buttons) {
+//Event Listener waits for the button to be clicked and the code inside will be runned
+        button.addEventListener("click", function() {
+//If statement to check the data type to see value. 
+//And  if it's submit button we're adding an Alert saying "You clicked submit"
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+//Using Teplate Literal, note that there is back quotes instead
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+
 function runGame() {
 
 }
@@ -31,5 +55,5 @@ function displayMultiplyQuestion() {
 }
 
 function displayDivisionQuestion() {
-    
+
 }
