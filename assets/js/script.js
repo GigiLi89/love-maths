bd64dc07e4a6dfc16af095c1b450b927b0c68d95
GigiLi89 <li.gigi89@gmail.com>
2024-03-13 10:08:49 +0000

    Added addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,26 +10,38 @@ document.addEventListener("DOMContentLoaded", function() {
         button.addEventListener("click", function() {
 //If statement to check the data type to see value. 
 //And  if it's submit button we're adding an Alert saying "You clicked submit"
-            if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+        if (this.getAttribute("data-type") === "submit") {
+            alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-//Using Teplate Literal, note that there is back quotes instead
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
-        })
+        });
     }
-})
+//Inside the event listner but outside the for loop
+    runGame("addition");
+});
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
 //Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+//Call for
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+//Error msg if it doesn't work
+    } else {
+        alert(`Unknown game type: $(gameType)`);
+//Throw statement stops the game and supplies error msg as well as print to debugging
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
+
+    //After writing the above about addition, go down to displayAdditionQuestion
 
 function checkAnswer() {
 
@@ -47,8 +59,12 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    //Gets the random numbers
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    //Sets the addition
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
